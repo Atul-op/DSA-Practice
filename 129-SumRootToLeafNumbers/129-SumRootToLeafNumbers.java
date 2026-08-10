91e964e615403216869d43638d63ba9abd23725f
Atul Gupta <81875557+Atul-op@users.noreply.github.com>
2026-08-10 16:17:19 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.java
@@ -0,0 +1,31 @@
+// Last updated: 10/08/2026, 16:17:19
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    private int helper(TreeNode root , int sum)
+    {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return sum*10+root.val;
+        return helper(root.left , sum*10+root.val)+helper(root.right , sum*10+root.val);
+    }
+    public int sumNumbers(TreeNode root)
+    {
+        return helper(root , 0);
+    }
+}
